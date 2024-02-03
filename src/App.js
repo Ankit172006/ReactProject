d15d18bb062ea5105151eea7aca48a110e0056a3
Karan <karantha289@gmail.com>
2024-02-03 19:16:19 +0530

    add first video in project

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
       <Nav/>
       <div className="App_main_contant_container">
 
-      <Main mainimg="https://i.ytimg.com/vi/HwvRa8FW7nM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC2_mc8mET-k13jPWfYHrU2lhDDzw" channellogo="https://yt3.ggpht.com/49eUE89RL69LuncBcF5xccRkff2fRlJytQuZ5vOgIzgOYLc5Bo9beP6uNCE_AOwpT2iqqC_ygq4=s68-c-k-c0x00ffffff-no-rj" videotitle="Ind Vs Eng, 2nd Test : Yashasvi Jaiswal की Superb Innings के ये हैं 3 गुनहगार | Rohit" channelname="News24 Sports" views="11k views" time="33 minutes ago" />
+      <Main posterimg="https://i.ytimg.com/vi/HwvRa8FW7nM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC2_mc8mET-k13jPWfYHrU2lhDDzw" channellogo="https://yt3.ggpht.com/49eUE89RL69LuncBcF5xccRkff2fRlJytQuZ5vOgIzgOYLc5Bo9beP6uNCE_AOwpT2iqqC_ygq4=s68-c-k-c0x00ffffff-no-rj" videotitle="Ind Vs Eng, 2nd Test : Yashasvi Jaiswal की Superb Innings के ये हैं 3 गुनहगार | Rohit" channelname="News24 Sports" views="11k views" time="33 minutes ago" />
       <br />
-      <Main/>
-      <Main/>
+      {/* <Main/>
+      <Main/> */}
       {/* <Main/>
       <Main/>
       <Main/>
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import video1 from "./video1.mp4"
 import "./Main.css"
 const Main = function main(Main){
   return (
     <div className='main_container'>
         <div className="upper_container">
             <div className="video_container">
-              <img className='mainimg' src={Main.mainimg} alt="" />
+            <video className='mainimg' poster={Main.posterimg} control >
+            <source src={video1} type="video/mp4"/>
+     </video>
+              {/* <img  src={Main.mainimg} alt="" /> */}
+              
             </div>
         </div>
       <div className="lower_container">
@@ -32,9 +37,14 @@ const Main = function main(Main){
                     <a href="">{Main.time}</a>
                   </div>
                 </div>
+
             </div>
         </div>
+  
       </div>
+      {/* <video src="./video/video1.mp4" type='mp4'></video> */}
+
+    
     </div>
   )
 }
